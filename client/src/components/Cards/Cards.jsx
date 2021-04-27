e57923976ef179998daaadd6c48a041161ca8a14
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-04-27 14:02:20 -0400

    add css styling to cards

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import CountUp from 'react-countup'
 import { Card, CardContent, Typography, Grid } from '@material-ui/core'
 import styles from './Cards.module.css';
+import cx from 'classnames';
 
 const Cards = (props) => {
 
@@ -19,7 +20,7 @@ const Cards = (props) => {
   return (
   <div class={styles.container}>
     <Grid container spacing={3} justify='center'>
-      <Grid item component={Card}>
+      <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
         <CardContent>
           <Typography color="textSecondary" gutterBottom>Infected</Typography>
           <Typography variant="h5" gutterBottom>
@@ -34,7 +35,7 @@ const Cards = (props) => {
           <Typography variant="body2">Number of active cases of COVID-19</Typography>
         </CardContent>
       </Grid>
-      <Grid item component={Card}>
+      <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
         <CardContent>
           <Typography color="textSecondary" gutterBottom>Recovered</Typography>
           <Typography variant="h5" gutterBottom>
@@ -49,7 +50,7 @@ const Cards = (props) => {
           <Typography variant="body2">Number of recoveries from COVID-19</Typography>
         </CardContent>
       </Grid>
-      <Grid item component={Card}>
+      <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
         <CardContent>
           <Typography color="textSecondary" gutterBottom>Deaths</Typography>
           <Typography variant="h5" gutterBottom>
