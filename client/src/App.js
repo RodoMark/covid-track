d52472d332abcaf9ef0517094e12573979985744
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-04-27 11:26:37 -0400

    add index.js with first three components

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Cards, Chart, CountryPicker } from './components'
+
+function App() {
+  return (
+    
+    <div className="App">
+
+    </div>
+    
+    
+  );
+}
+
+export default App
