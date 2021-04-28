e2b46482262c7ea98d64fec8a726eb7e4f083b1d
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-04-28 14:46:30 -0400

    add App and Index

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Cards, TrackerChart, CountryPicker } from './components'
+import styles from './App'
+import { fetchData } from './api'
+
+class App extends React.Component {
+  state = {
+    data: {},
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData() 
+    this.setState({ data: fetchedData })
+  }
+ 
+  render() {
+    const { data } = this.state; 
+
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <TrackerChart />
+        <CountryPicker />
+      </div>
+    
+    );
+  }
 }
 
-export default App;
+export default App
